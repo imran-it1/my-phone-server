67a27212e828be4d0041eb14cebee988799b7d59
Imran Hossain Sakil <112363382+imran-it1@users.noreply.github.com>
2023-10-10 15:55:50 +0600

    import the phones json api and set router for all phones data '/phones' and get signle phone data using dynamic routing and set dynamic route '/phones/:id' use params from user requiest side to get exact id and find the exact phone and send it as requiest.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const express = require("express");
+// import local phones json data
+const phones = require("./phones.json");
 const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+	res.send("Hello from my phonesssssssss serverrrr");
+});
+
+// Send all phones data
+app.get("/phones", (req, res) => {
+	res.send(phones);
+});
+
+// Send single phone data
+
+app.get("/phones/:id", (req, res) => {
+	// Get id from user side thats mean from requeest (req.params)
+	const id = parseInt(req.params.id); // *** req.params.id *** eto tuku jeta return kore seta string, kintu phone er id gulo number format a ace. Tai etake parseInt kore niye number a convert korte hobe
+
+	// id diye single phone take ber korte hobe
+	const singlePhone = phones?.find(phone => phone.id === id) || {}; // flase hole {} object dekha jabe
+
+	// Single phone er data ke eibar response akare send kore dite hobe user er kace
+	res.send(singlePhone);
+});
+
+app.listen(port, () => {
+	console.log(`My phone server is listening on port ${port}`);
+});
